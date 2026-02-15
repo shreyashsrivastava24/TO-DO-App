1cc9f19875a7987e1ae06d1d3ea89d1b42f0c710
shreyashsrivastava24 <shreyashsrivastava010@gmail.com>
2026-02-15 23:57:22 +0530

    added clear button

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-indigo-900 text-white py-4 text-center'>
+      <p>© 2026 iTask - All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // in files me ye tailwind config dhundhega
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
